acbb7fc6dfbc415f5c8ec51f9a0a3a3740026f0b
MislavSag <mislav.sagovac@contentio.biz>
2024-01-26 09:50:38 +0100

    bug fix in file

--- download.R
+++ download.R
@@ -5,4 +5,4 @@ blob_key = readLines('./blob_key.txt')
 endpoint = "https://snpmarketdata.blob.core.windows.net/"
 BLOBENDPOINT = storage_endpoint(endpoint, key=blob_key)
 cont = storage_container(BLOBENDPOINT, "jphd")
-storage_download(cont, "zse-predictors-20240112.csv", overwrite=TRUE)
+storage_download(cont, "zse-predictors-20240117.csv", overwrite=TRUE)
