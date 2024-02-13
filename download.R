8a71794d947f7bf1e215cc385106fe29673a78fe
MislavSag <mislav.sagovac@contentio.biz>
2024-02-13 12:37:19 +0100

    week hotizont

--- download.R
+++ download.R
@@ -5,5 +5,5 @@ blob_key = readLines('./blob_key.txt')
 endpoint = "https://snpmarketdata.blob.core.windows.net/"
 BLOBENDPOINT = storage_endpoint(endpoint, key=blob_key)
 cont = storage_container(BLOBENDPOINT, "jphd")
-storage_download(cont, "zse-predictors-20240126.csv", overwrite=TRUE)
+storage_download(cont, "zse-predictors-20240129.csv", overwrite=TRUE)
 s
